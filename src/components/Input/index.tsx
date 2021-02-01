a384b578124fd2450dfec07396989b289453256f
Jonatas Falkaniere <jonatas.falkaniere@ngosolucoes.com>
2021-02-01 17:25:38 -0300

    change input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { TextInputProps } from 'react-native';
 
-import { Container, TextInput,  } from './styles';
+import { Container, TextInput } from './styles';
 
 const Input: React.FC = () => {
-  return(
+  return (
     <Container>
       {/* <Icon /> */}
       <TextInput />
     </Container>
   );
-}
+};
 
 export default Input;
